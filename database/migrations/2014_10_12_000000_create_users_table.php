7616720df0faa6ab8e41892a4d2a5ac716bd4792
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2024-01-05 15:51:49 +0530

    admin credentials

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 class CreateUsersTable extends Migration
 {
@@ -22,6 +24,14 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        // Insert the default admin user
+        DB::table('users')->insert([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@agriwings.com',
+            'password' => bcrypt('123456'), // Change 'password' to the desired password
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
