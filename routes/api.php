f002668b79ddbf32745991ae8925d8caa07c36d2
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2024-01-09 11:46:25 +0530

    code changes for user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\ApI\UserController;
 use App\Http\Controllers\FarmerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
