e5b5848d252c14a8dd79a4299a8bd184cdf092e4
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2024-02-22 16:02:10 +0530

    unauthenticated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            // Handle the authentication exception here
+            // return response()->json(['error' => 'Unauthenticated'], 401);
+            $result_array = array(
+                'status' => 'error',
+                'statuscode' => '401',
+                'msg' => 'User not Authenticated.',
+                'data' => []
+            );
+
+            return response()->json($result_array, 200);
+        }
+
+        return parent::render($request, $exception);
     }
 }
