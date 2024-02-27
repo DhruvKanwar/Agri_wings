aeadd612747c4ce0b968d0f4aa39347c9c7a4c45
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2024-02-27 15:51:26 +0530

    scheme changes

--- app/Http/Controllers/AssetOperatorController.php
+++ app/Http/Controllers/AssetOperatorController.php
@@ -633,20 +633,26 @@ class AssetOperatorController extends Controller
                 $orderType = $check_order_exists->order_type;
 
                 if ($orderType == 1) {
-                    $applicableSchemes = Scheme::withTrashed()->select('id', 'type', 'client_id', 'scheme_name', 'discount_price')->whereIn('type', [1, 2, 3])
-                        ->where(function ($query) use ($clientId) {
-                            $query->where('client_id', $clientId)
-                                ->orWhereNull('client_id')
-                                ->orWhere('client_id', ''); // Add this condition
-                        })
-                        ->where('crop_id', $cropId)
-                        ->where('period_from', '<=', $orderDate)
-                        ->where('period_to', '>=', $orderDate)
-                        ->where('min_acreage', '<=', (int)$requestedAcreage)
-                        ->where('max_acreage', '>=', (int)$requestedAcreage)
-                        ->orderBy('id', 'desc')
+                    $applicableSchemes = Scheme::withTrashed()
+                    ->select('id', 'type', 'client_id', 'scheme_name', 'discount_price')
+                    ->whereIn('type', [1, 2, 3])
+                    ->where(function ($query) use ($clientId) {
+                        $query->where('client_id', $clientId)
+                        ->orWhereNull('client_id')
+                        ->orWhere('client_id', ''); // Add this condition
+                    })
+                    ->where('crop_id', $cropId)
+                    ->where('period_from', '<=', $orderDate)
+                    ->where('period_to', '>=', $orderDate)
+                    ->where(function ($query) use ($orderDate) {
+                        $query->whereNull('deleted_at') // Records not deleted
+                        ->orWhere('deleted_at', '>=', $orderDate); // Records deleted after the given date
+                    })
+                    ->where('min_acreage', '<=', (int)$requestedAcreage)
+                    ->where('max_acreage', '>=', (int)$requestedAcreage)
+                        // ->orderBy('id', 'desc')
                         // ->where('status', 1)
-                        ->first();
+                        ->get();
 
                     // return $applicableSchemes;
 
@@ -708,16 +714,34 @@ class AssetOperatorController extends Controller
                 // check_again
                 elseif ($orderType == 4 || $orderType == 5) {
 
-                    $applicableSchemes = Scheme::withTrashed()->select('id', 'type', 'crop_base_price', 'scheme_name', 'discount_price')->where('type', $orderType)
+                    // $applicableSchemes = Scheme::withTrashed()->select('id', 'type', 'crop_base_price', 'scheme_name', 'discount_price')->where('type', $orderType)
+                    //     ->where('client_id', $clientId)
+                    //     ->where('crop_id', $cropId)
+                    //     ->where('period_from', '<=', $orderDate)
+                    //     ->where('period_to', '>=', $orderDate)
+                    //     ->where('min_acreage', '<=', (int)$requestedAcreage)
+                    //     ->where('max_acreage', '>=', (int)$requestedAcreage)
+                    //     // ->orderBy('id','desc')
+                    //     // ->where('status', 1)
+                    //     ->first();
+
+                    $applicableSchemes = Scheme::withTrashed()
+                        ->select('id', 'type', 'crop_base_price', 'scheme_name', 'discount_price')->where('type', $orderType)
                         ->where('client_id', $clientId)
                         ->where('crop_id', $cropId)
                         ->where('period_from', '<=', $orderDate)
                         ->where('period_to', '>=', $orderDate)
                         ->where('min_acreage', '<=', (int)$requestedAcreage)
                         ->where('max_acreage', '>=', (int)$requestedAcreage)
-                        ->orderBy('id','desc')
+                        ->where(function ($query) use ($orderDate) {
+                            $query->whereNull('deleted_at') // Records not deleted
+                                ->orWhere('deleted_at', '>=', $orderDate); // Records deleted after the given date
+                        })
+                    
+                        // ->orderBy('id', 'desc')
                         // ->where('status', 1)
-                        ->first();
+                        ->get();
+
 
                     // return $applicableSchemes;
 
